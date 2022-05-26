7dfc0be2a99bf651ce7e9ee37f2d865dcbfaaf9f
Apprenant 08 <apprenant08@macbook-pro-de-apprenant.home>
2022-05-26 08:26:01 +0200

    first commit

---
+++ TutoGit/TutoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutoGitApp.swift
+//  TutoGit
+//
+//  Created by Apprenant 08 on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TutoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
